66ca52e7da7195c287a758f94a1977d62807a51c
unknown <aigege02@aigege.game>
2025-07-25 20:30:15 +0800

    a

--- Source/CustomWidget/MyWidget03/MyUWidget03.h
+++ Source/CustomWidget/MyWidget03/MyUWidget03.h
@@ -13,37 +13,6 @@
 UCLASS()
 class CUSTOMWIDGET_API UMyUWidget03 : public UPanelWidget
 {
-	GENERATED_UCLASS_BODY()
-
-public:
-
-	/**  */
-	UFUNCTION(BlueprintCallable, Category="Panel")
-	UMyUSlot03* AddChildToVerticalBox(UWidget* Content);
-
-#if WITH_EDITOR
-	// UWidget interface
-	virtual const FText GetPaletteCategory() override;
-	// End UWidget interface
-#endif
-
-protected:
-
-	// UPanelWidget
-	virtual UClass* GetSlotClass() const override;
-	virtual void OnSlotAdded(UPanelSlot* Slot) override;
-	virtual void OnSlotRemoved(UPanelSlot* Slot) override;
-	// End UPanelWidget
-
-	virtual void ReleaseSlateResources(bool bReleaseChildren) override;
-
-protected:
-
-	TSharedPtr<class SMySWidget03> MySWidget03;
-
-protected:
-	// UWidget interface
-	virtual TSharedRef<SWidget> RebuildWidget() override;
-	// End of UWidget interface
-
+	GENERATED_BODY()
+	
 };
